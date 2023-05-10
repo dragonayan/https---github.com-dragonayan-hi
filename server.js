08bac15140fdda69fed3629b423c622b90cc0faf
dragonayan <94789365+dragonayan@users.noreply.github.com>
2023-05-11 00:27:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,12 +6,13 @@ app.use(express.json());
 const userRoute = require("./routes/userRoute");
 const adminRoute = require("./routes/adminRoute");
 const doctorRoute = require("./routes/doctorsRoute");
-
+const path= require("path")
 
 app.use("/api/user", userRoute);
 app.use("/api/admin", adminRoute);
 app.use("/api/doctor", doctorRoute);
-
+app.use(express.static(path.join(__dirname,'./client/build')));
+app.get('*',(req, res)=> res.sendFile(path.join(__dirname,'./client/build/index.html')));
 
 const port = process.env.PORT || 5000;
 app.get("/", (req, res) => res.send("well done Ayan"));
